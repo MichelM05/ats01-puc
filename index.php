26e736192e94a556ad43d1c826194c03dbc72cb4
Michel Martins <dgm.michell@gmail.com>
2025-03-31 19:48:17 -0300

    Update index.php
    fix: Adicionar validação de entrada

--- index.php
+++ index.php
@@ -24,6 +24,11 @@
 </html>
 
 <?php
+if (!is_numeric($num1) || !is_numeric($num2)) {
+    echo "<h3>Erro: insira apenas números válidos!</h3>";
+    exit;
+}
+
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $num1 = $_POST["num1"];
     $num2 = $_POST["num2"];
